a6d1ccd28318e49b10a94a3e2c19a17f6597b00b
MichalZdanuk <zdanuk_m@wp.pl>
2025-04-03 19:07:20 +0200

    Implement user reregistered notification for conference.

---
+++ src/Confy/Confy.Application/EventHandlers/UserReRegisteredForConferenceEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using Confy.Application.Exceptions;
+using Confy.Application.Factories;
+using Confy.Domain.Authentication.Repositories;
+using Confy.Domain.Notification.Repositories;
+using Confy.Domain.Registration.Events;
+using Microsoft.Extensions.Logging;
+
+namespace Confy.Application.EventHandlers;
+public class UserReRegisteredForConferenceEventHandler(INotificationRepository notificationRepository,
+	IUserRepository userRepository,
+	INotificationFactory notificationFactory,
+	INotificationSenderService notificationSenderService,
+	ILogger<UserReRegisteredForConferenceEventHandler> logger)
+	: INotificationHandler<UserReRegisteredForConferenceEvent>
+{
+	public async Task Handle(UserReRegisteredForConferenceEvent domainEvent, CancellationToken cancellationToken)
+	{
+		var user = await userRepository.GetByIdAsync(domainEvent.UserId);
+
+		if (user is null)
+		{
+			throw new UserNotFoundException(domainEvent.UserId);
+		}
+
+		var notification = notificationFactory.CreateNotification(domainEvent.UserId,
+			domainEvent.ConferenceId,
+			Domain.Notification.Enums.NotificationType.Registration,
+			domainEvent.ConferenceName);
+
+		await notificationRepository.AddAsync(notification);
+
+		var notificationPayload = notification.MapToPayload(user.Email);
+
+		await notificationSenderService.SendNotification(notificationPayload);
+	}
+}
