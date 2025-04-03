adb877abfdbad9324adbbd6bcd7ddcb59eec5548
MichalZdanuk <zdanuk_m@wp.pl>
2025-04-03 19:01:31 +0200

    Implement notification for cancel registration on conference.

---
+++ src/Confy/Confy.Application/EventHandlers/RegistrationForConferenceCanceledEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using Confy.Application.Exceptions;
+using Confy.Application.Factories;
+using Confy.Domain.Authentication.Repositories;
+using Confy.Domain.Notification.Repositories;
+using Confy.Domain.Registration.Events;
+using Microsoft.Extensions.Logging;
+
+namespace Confy.Application.EventHandlers;
+public class RegistrationForConferenceCanceledEventHandler(INotificationRepository notificationRepository,
+	IUserRepository userRepository,
+	INotificationFactory notificationFactory,
+	INotificationSenderService notificationSenderService,
+	ILogger<RegistrationForConferenceCanceledEventHandler> logger)
+	: INotificationHandler<RegistrationForConferenceCanceledEvent>
+{
+	public async Task Handle(RegistrationForConferenceCanceledEvent domainEvent, CancellationToken cancellationToken)
+	{
+		var user = await userRepository.GetByIdAsync(domainEvent.UserId);
+
+		if (user is null)
+		{
+			throw new UserNotFoundException(domainEvent.UserId);
+		}
+
+		var notification = notificationFactory.CreateNotification(domainEvent.UserId,
+			domainEvent.ConferenceId,
+			Domain.Notification.Enums.NotificationType.RegistrationCanceled,
+			domainEvent.ConferenceName);
+
+		await notificationRepository.AddAsync(notification);
+
+		var notificationPayload = notification.MapToPayload(user.Email);
+
+		await notificationSenderService.SendNotification(notificationPayload);
+	}
+}
