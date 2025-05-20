9bcf2ea6be3ccd36733a966f251b529f453ef13a
MichalZdanuk <zdanuk_m@wp.pl>
2025-05-20 13:57:48 +0200

    Add seeding conference.

--- src/Confy/Confy.API/Extensions.cs
+++ src/Confy/Confy.API/Extensions.cs
@@ -45,5 +45,6 @@ public static class Extensions
 	private static async Task SeedAsync(ConfyDbContext confyDbContext)
 	{
 		await AdminUserSeeder.SeedAdminUserAsync(confyDbContext);
+		await ConferenceSeeder.SeedConferenceAsync(confyDbContext);
 	}
 }
---
+++ src/Confy/Confy.Infrastructure/Seeding/ConferenceSeeder.cs
@@ -0,0 +1,45 @@
+﻿using Confy.Domain.ConferenceManagement.Entities;
+using Confy.Domain.ConferenceManagement.ValueObjects;
+using Confy.Shared.Enums;
+
+namespace Confy.Infrastructure.Seeding;
+public static class ConferenceSeeder
+{
+	private const string _conferenceId = "BFA9B907-4EA3-437B-9380-B60639F8BEAC";
+
+	public static async Task SeedConferenceAsync(ConfyDbContext context)
+	{
+		var conference = await context.Conferences
+			.FirstOrDefaultAsync(u => u.Id == Guid.Parse(_conferenceId));
+
+		if (conference is null)
+		{
+			var newConference = Conference.Create(
+				Guid.Parse(_conferenceId),
+				".NET Summit 2025",
+				ConferenceLanguage.English,
+				ConferenceLinks.Of(
+					"https://www.net-summit2025.com",
+					"https://facebook.com/netsummit2025",
+					"https://instagram.com/netsummit2025"
+				),
+				ConferenceDetails.Of(
+					new DateTime(2025, 6, 6),
+					new DateTime(2025, 6, 8),
+					"The premier .NET community event featuring top speakers, workshops, and networking opportunities.",
+					false // not online
+				),
+				Address.Of(
+					"Berlin",
+					"Germany",
+					"Berlin Congress Center, Alexanderstraße 11",
+					"10178"
+				)
+			);
+
+			await context.Conferences.AddAsync(newConference);
+			await context.SaveChangesAsync();
+		}
+	}
+}
+
